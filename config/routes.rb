d6b3dccd5285a622830092bd2ed41fca43af96cc
Caitlin Prentke <caitlin.prentke@gmail.com>
2019-03-09 12:35:54 -0500

    nest money records and categories under users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:create, :show]
-  resources :categories, only: [:create, :edit, :delete]
-  resources :money_records, only: [:index, :create, :edit, :show, :delete]
+  resources :users, only: [:create, :show] do
+    resources :categories, only: [:create, :edit, :delete]
+    resources :money_records, only: [:index, :create, :edit, :show, :delete]
+  end
 
 end
